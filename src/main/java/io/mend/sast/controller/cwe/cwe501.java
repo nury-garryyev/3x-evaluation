8d7a45b2a73bcd9028bf2dc0a2012ce495fe14b5
nury-garryyev <nury.garryyev@gmail.com>
2022-12-30 12:50:13 +0500

    sd

--- src/main/java/io/mend/sast/controller/cwe/cwe501.java
+++ src/main/java/io/mend/sast/controller/cwe/cwe501.java
@@ -2,8 +2,6 @@ package io.mend.sast.controller.cwe;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,21 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/cwe501")
 public class cwe501 {
 
-    private static final Logger logger = LoggerFactory.getLogger(cwe501.class);
-
     @GetMapping(value = "/session")
-    public void session(HttpServletRequest request, HttpSession session) {
+    public void session(HttpServletRequest request, HttpSession sessionArgument) {
+        HttpSession sessionRequest = request.getSession();
+
         String input = request.getParameter("input");
-        String a = request.getParameter("a");
-        String b = request.getParameter("b");
 
-        session.setAttribute("input", input);
-        session.setAttribute(input, "input");
-        session.setAttribute(a, b);
+        sessionArgument.setAttribute("input", input);
+        sessionArgument.setAttribute(input, "input");
 
-        HttpSession session1 = request.getSession();
-        session1.setAttribute("input", input);
-        session1.setAttribute(input, "input");
-        session1.setAttribute(a, b);
+        sessionRequest.setAttribute("input", input);
+        sessionRequest.setAttribute(input, "input");
     }
 }
