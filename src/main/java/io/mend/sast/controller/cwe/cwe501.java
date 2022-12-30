dff757ee6ab70fe076936706ef68572d68c02121
nury-garryyev <nury.garryyev@gmail.com>
2022-12-30 11:36:53 +0500

    sd

---
+++ src/main/java/io/mend/sast/controller/cwe/cwe501.java
@@ -0,0 +1,27 @@
+package io.mend.sast.controller.cwe;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cwe501")
+public class cwe501 {
+
+    private static final Logger logger = LoggerFactory.getLogger(cwe501.class);
+
+    @GetMapping(value = "/session")
+    public void session(HttpServletRequest request, HttpSession session) {
+        String input = request.getParameter("input");
+        String a = request.getParameter("a");
+        String b = request.getParameter("b");
+
+        session.setAttribute("input", input);
+        session.setAttribute(input, "input");
+        session.setAttribute(a, b);
+    }
+}
